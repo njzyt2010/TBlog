7cac2bfe87db48cce9c53b621c63be37ad0b42e9
njzyt2010 <njzyt2010@163.com>
2022-03-09 21:39:38 +0800

    增加注释

--- internal/modules/Model.go
+++ internal/modules/Model.go
@@ -3,10 +3,10 @@ package modules
 import "time"
 
 type Model struct {
-	Id          uint64    `gorm:"primary_key" json:"id"`
-	CreatedBy   string    ` json:"createdBy"`
-	UpdateBy    string    `json:"updateBy"`
-	CreatedTime time.Time `json:"createdTime"`
-	UpdateTime  time.Time `json:"updateTime"`
-	Deleted     uint8     `gorm:"column:deleted_" json:"-"`
+	Id          uint64    `gorm:"primary_key" json:"id"`    // 主键
+	CreatedBy   string    `json:"createdBy"`               // 创建人
+	UpdateBy    string    `json:"updateBy"`                 // 变更人
+	CreatedTime time.Time `json:"createdTime"`              // 创建时间
+	UpdateTime  time.Time `json:"updateTime"`               // 变更时间
+	Deleted     uint8     `gorm:"column:deleted_" json:"-"` // 删除标志
 }
