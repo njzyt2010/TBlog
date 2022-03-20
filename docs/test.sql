c58b2b87250f4cbc065f5e05a9e46fec08b798ba
njzyt2010 <njzyt2010@163.com>
2022-03-20 21:30:55 +0800

    增加测试数据

--- docs/test.sql
+++ docs/test.sql
@@ -9,5 +9,23 @@ values  (1, 0, '首页', 1, 0, '0', '0', null, null),
         (8, 5, 'Spring Cloud', 8, 0, '0', '0', null, null);
 
 
-insert into t_article(title_, content_, reprint_, reprint_url, topic_id, published_, deleted_) VALUES
-("测试文章","测试的内容",0,'',1,1,0)
+insert into tblog.t_article (id, title_, content_, reprint_, reprint_url, topic_id, published_, deleted_, created_by, update_by, created_time, update_time)
+values
+
+(1, '测试文章', '测试的内容', 0, '', 4, 1, 0, '0', '0', null, null),
+(2, '测试文章', '测试的内容', 0, '', 4, 1, 0, '0', '0', null, null),
+(3, '测试文章', '测试的内容', 0, '', 4, 1, 0, '0', '0', null, null),
+(4, '测试文章', '测试的内容', 0, '', 4, 1, 0, '0', '0', null, null),
+(5, '测试文章', '测试的内容', 0, '', 4, 1, 0, '0', '0', null, null),
+(6, '测试文章', '测试的内容', 0, '', 4, 1, 0, '0', '0', null, null),
+(7, '测试文章', '测试的内容', 0, '', 4, 1, 0, '0', '0', null, null),
+(8, '测试文章', '测试的内容', 0, '', 4, 1, 0, '0', '0', null, null),
+(9, '测试文章', '测试的内容', 0, '', 5, 1, 0, '0', '0', null, null),
+(10, '测试文章', '测试的内容', 0, '', 3, 1, 0, '0', '0', null, null),
+(11, '测试文章', '测试的内容', 0, '', 3, 1, 0, '0', '0', null, null),
+(12, '测试文章', '测试的内容', 0, '', 3, 1, 0, '0', '0', null, null),
+(13, '测试文章', '测试的内容', 0, '', 3, 1, 0, '0', '0', null, null),
+(14, '测试文章', '测试的内容', 0, '', 3, 1, 0, '0', '0', null, null),
+(15, '测试文章', '测试的内容', 0, '', 3, 1, 0, '0', '0', null, null);
+
+
